3730b3472624abec3f6a70e6c9bc959b86ec827e
Toni Butkovic <tonibutkovic23@gmail.com>
2014-12-10 16:39:03 +0100

    deploy symlink, translation compiling .po files

--- module/Administration/src/Administration/Controller/TranslationController.php
+++ module/Administration/src/Administration/Controller/TranslationController.php
@@ -65,8 +65,10 @@ class TranslationController extends AbstractActionController {
 
                 $translateFile = $post['translation-file'];
                 $target_file = $globalConfig['languageDir'] . basename($fileName);
+                $newMoFile = $globalConfig['languageDir'] . basename($locale . '.mo');
 
                 if (move_uploaded_file($translateFile['tmp_name'], $target_file)) {
+                    exec('msgfmt -cv -o ' . $newMoFile . ' ' . $target_file);
                     $this->flashMessenger()->addMessage('The file ' . basename($translateFile['name']). ' has been uploaded.
                      Its name is changed to ' . basename($fileName));
                 } else {

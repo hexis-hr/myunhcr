2441974d222d5cc2f127d9aa9e8200b2bc8d6416
Toni Butkovic <tonibutkovic23@gmail.com>
2014-12-16 14:15:46 +0100

    deploy fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,8 +29,8 @@ set :deploy_to, '/var/www/myunhcr/'
 # Default value for :scm is :git
 set :scm, :git
 
-before "deploy:finished", "deploy:install"
 before "deploy:finished", "deploy:symlink_shared"
+before "deploy:finished", "deploy:install"
 
 namespace :deploy do
 
@@ -39,15 +39,15 @@ namespace :deploy do
         on "deploy@dev.hexis.hr" do
             execute "cd #{current_path} && composer update"
             execute "cd #{current_path} && php zf.php orm:schema-tool:update --force"
-            execute "cd #{current_path} && chown www-data:www-data /data/DoctrineORMModule/Proxy"
-            execute "cd #{current_path} && chown www-data:www-data /module/Application/language"
+            execute :sudo, :chown, "-R www-data:www-data #{current_path}/data/DoctrineORMModule/Proxy/"
+            execute :sudo, :chown, "-R www-data:www-data #{current_path}/module/Application/language/"
         end
     end
 
     desc "Symlink shared configs and folders on each release."
       task :symlink_shared do
         on "deploy@dev.hexis.hr" do
-            execute "ln -nfs #{shared_path}/config/doctrine.local.php #{current_path}/config/autoload/doctrine.local.php"
+            execute "ln -nfs #{shared_path}/config/doctrine.local.php /var/www/myunhcr/current/config/autoload/doctrine.local.php"
         end
       end
 end

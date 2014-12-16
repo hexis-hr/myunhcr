0607ce9d560c705862322f5ea4e56bd83f1ae338
Toni Butkovic <tonibutkovic23@gmail.com>
2014-12-16 12:20:08 +0100

    deploy fix 4

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,21 +27,3 @@ set :deploy_to, '/var/www/myunhcr/'
 
 # Default value for :scm is :git
 set :scm, :git
-
-before "deploy:restart", "deploy:install"
-
-namespace :deploy do
-
-    desc "run composer update"
-    task :install do
-#        run "cd #{current_path} && composer install"
-#        run "cd #{current_path} && php zf.php orm:schema-tool:update --force"
-        run "cd #{current_path} && chown www-data:www-data /data/DoctrineORMModule/Proxy"
-        run "cd #{current_path} && chown www-data:www-data /module/Application/language"
-    end
-
-    desc "Symlink shared configs and folders on each release."
-      task :symlink_shared do
-        run "ln -nfs #{shared_path}/config/doctrine.local.php #{current_path}/config/autoload/doctrine.local.php"
-      end
-end

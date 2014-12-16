4350187375cdf660df480ffd7d50c4a4a8e32ea2
Toni Butkovic <tonibutkovic23@gmail.com>
2014-12-16 13:50:38 +0100

    deploy fix 7

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,8 +16,8 @@ set :writable_dirs, ["application/logs"]
 
 set(:symlinks, [
   {
-    source: "/public/uploads",
-    link: "/data/uploads"
+    source: "/var/www/myunhcr/current/public/uploads",
+    link: "/var/www/myunhcr/current/data/uploads"
   }
 ])
 
@@ -29,20 +29,25 @@ set :deploy_to, '/var/www/myunhcr/'
 # Default value for :scm is :git
 set :scm, :git
 
-before "deploy:restart", "deploy:install"
+before "deploy:finished", "deploy:install"
+before "deploy:finished", "deploy:symlink_shared"
 
 namespace :deploy do
 
     desc "run composer update"
     task :install do
-        run "cd #{current_path} && composer update"
-        run "cd #{current_path} && php zf.php orm:schema-tool:update --force"
-        run "cd #{current_path} && chown www-data:www-data /data/DoctrineORMModule/Proxy"
-        run "cd #{current_path} && chown www-data:www-data /module/Application/language"
+        on "deploy@dev.hexis.hr" do
+            execute "cd #{current_path} && composer update"
+            execute "cd #{current_path} && php zf.php orm:schema-tool:update --force"
+            execute "cd #{current_path} && chown www-data:www-data /data/DoctrineORMModule/Proxy"
+            execute "cd #{current_path} && chown www-data:www-data /module/Application/language"
+        end
     end
 
     desc "Symlink shared configs and folders on each release."
       task :symlink_shared do
-        run "ln -nfs #{shared_path}/config/doctrine.local.php #{current_path}/config/autoload/doctrine.local.php"
+        on "deploy@dev.hexis.hr" do
+            execute "ln -nfs #{shared_path}/config/doctrine.local.php #{current_path}/config/autoload/doctrine.local.php"
+        end
       end
 end

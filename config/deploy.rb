212ce8a79ec35b90eaa313a2e7a30789ef10cd6f
Toni Butkovic <tonibutkovic23@gmail.com>
2014-12-16 12:35:51 +0100

    deploy fix 5

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,7 @@ set :permission_method,     :acl
 set :webserver_user,        "www-data"
 
 set :use_set_permissions,   true
+set :use_sudo,   true
 
 set :linked_dirs, %w{application/logs vendor}
 
@@ -27,3 +28,21 @@ set :deploy_to, '/var/www/myunhcr/'
 
 # Default value for :scm is :git
 set :scm, :git
+
+before "deploy:restart", "deploy:install"
+
+namespace :deploy do
+
+    desc "run composer update"
+    task :install do
+        run "cd #{current_path} && composer update"
+        run "cd #{current_path} && php zf.php orm:schema-tool:update --force"
+        run "cd #{current_path} && chown www-data:www-data /data/DoctrineORMModule/Proxy"
+        run "cd #{current_path} && chown www-data:www-data /module/Application/language"
+    end
+
+    desc "Symlink shared configs and folders on each release."
+      task :symlink_shared do
+        run "ln -nfs #{shared_path}/config/doctrine.local.php #{current_path}/config/autoload/doctrine.local.php"
+      end
+end
